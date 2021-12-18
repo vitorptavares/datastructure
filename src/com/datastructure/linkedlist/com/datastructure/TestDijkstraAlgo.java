5414e49b369704c762ce938bac4d7f28e8603ecd
vitortavares <vitortavares984@gmail.com>
2021-12-18 13:36:33 -0300

    Following datastructure code examples added
    *LinkedList
    *ArrayList
    *DoubleLiskedList
    *Stack
    *DikstraAlgorithm

---
+++ src/com/datastructure/linkedlist/com/datastructure/TestDijkstraAlgo.java
@@ -0,0 +1,10 @@
+package com.datastructure.linkedlist.com.datastructure;
+
+import com.datastructure.stack.DijkstraAlgo;
+
+public class TestDijkstraAlgo {
+    public static void main(String[] args) {
+        DijkstraAlgo dijkstraAlgo = new DijkstraAlgo();
+        dijkstraAlgo.interpretExpression("((1+3)+(2*5))");
+    }
+}
---
+++ src/com/datastructure/stack/DijkstraAlgo.java
@@ -0,0 +1,41 @@
+package com.datastructure.stack;
+
+import java.util.Stack;
+
+public class DijkstraAlgo {
+    private Stack<Character> operationStack;
+    private Stack<Double> valueStack;
+
+    public DijkstraAlgo() {
+        this.operationStack = new Stack<>();
+        this.valueStack = new Stack<>();
+    }
+
+    public void interpretExpression(String expression){
+        char[] expressionArray = expression.toCharArray();
+
+        for (char c :
+                expressionArray) {
+            //if c== '(' do nothing
+            if(c=='('){
+
+            }
+            else if(c=='+' || c=='*'){
+                operationStack.push(c);
+            }
+            else if(c==')'){
+                Character operation = operationStack.pop();
+                if(operation.equals('*')){
+                    valueStack.push(valueStack.pop() * valueStack.pop());
+                }
+                if(operation.equals('+')){
+                    valueStack.push(valueStack.pop() + valueStack.pop());
+                }
+            }
+            else{
+                valueStack.push(Double.valueOf(String.valueOf(c)));
+            }
+        }
+        System.out.println(valueStack.pop());
+    }
+}
